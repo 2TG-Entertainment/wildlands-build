ffe1105b3389a68df9feadc6b491d993f0c6aac0
Aba <abanoori33@gmail.com>
2026-01-15 03:26:42 +0300

    0.8.15 yellow test build

--- Build/Hunters.loader.js
+++ Build/Hunters.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 1,
     },
-    wasmFileSize: 108678203,
+    wasmFileSize: 108707568,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
