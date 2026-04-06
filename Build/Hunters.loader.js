7ba456e90ae2bb6715f593391d28f6fc2201a82d
Aba <abanoori33@gmail.com>
2026-04-06 23:36:35 +0300

    0.9.9 yellow meadow test

--- Build/Hunters.loader.js
+++ Build/Hunters.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 1,
     },
-    wasmFileSize: 111545056,
+    wasmFileSize: 111545781,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
